c9a718e5354750876a32a9960502adcbe480d423
cuongdnqb5@gmail.com <cuongnguyen@cuong-nguyen.lan>
2023-05-23 22:39:16 +0700

    Topic 56 - Framework 05 - Build BasePage class - Part I

--- actions/commons/BasePage.java
+++ actions/commons/BasePage.java
@@ -1,5 +1,116 @@
 package commons;
 
+import java.util.Set;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class BasePage {
+	
+	// Hàm dùng để làm gì
+	// Dùng cái hàm nào của Selenium
+	// Kiểu trả về của hàm đó
+	// Các hàm mà tương tác thì hầu như là void: Click/ senkeys/ accept/ cancel/ select/...
+	// Các hàm dùng để lấy dữ liệu ra thì hầu như là String/ int/ WebElement/ List<WebElement>
+	// Hàm Void không cần return
+	// Kiểu dữ liệu return về khớp với kiểu dữ liệu của hàm
+	
+	/* Web Browser */	
+	public void openPageUrl(WebDriver driver, String pageUrl) {
+		driver.get(pageUrl);
+	}
+	
+	public String getPageTitle(WebDriver driver) {
+		return driver.getTitle();
+	}
+	
+	public String getCurrentPageUrl(WebDriver driver) {
+		return driver.getCurrentUrl();
+	}
+	
+	public String getPageSourceCode(WebDriver driver) {
+		return driver.getPageSource();
+	}
+	
+	public void backToPage(WebDriver driver) {
+		driver.navigate().back();
+	}
+	
+	public void forwardToPage(WebDriver driver) {
+		driver.navigate().forward();
+	}
+	
+	public void refreshToPage(WebDriver driver) {
+		driver.navigate().refresh();
+	}
+	
+	public Alert waitForAlertPresence(WebDriver driver) {
+		return new WebDriverWait(driver, 30).until(ExpectedConditions.alertIsPresent());
+	}
+	
+	public void acceptToAlert(WebDriver driver) {
+		waitForAlertPresence(driver).accept();
+	}
+	
+	public void cancelToAlert(WebDriver driver) {
+		waitForAlertPresence(driver).dismiss();
+	}
+	
+	public String getTextToAlert(WebDriver driver) {
+		return waitForAlertPresence(driver).getText();
+	}
+	
+	public void sendkeyToAlert(WebDriver driver, String keyToSend) {
+		waitForAlertPresence(driver).sendKeys(keyToSend);
+	}
+	
+	public void switchToWindowByID(WebDriver driver, String parentID) {
+		Set<String> allWindowIDs = driver.getWindowHandles();
+
+		for (String id : allWindowIDs) {
+			if (!id.equals(parentID)) {
+				driver.switchTo().window(id);
+				sleepInsecond(3);
+			}
+		}
+	}
+
+	public void switchToWindowByTitle(WebDriver driver, String expectedTitle) {
+		Set<String> allWindowIDs = driver.getWindowHandles();
+		for (String id : allWindowIDs) {
+			driver.switchTo().window(id);
+			String currentPageTitle = driver.getTitle();
+			if (currentPageTitle.equals(expectedTitle)) {
+				break;
+
+			}
+
+		}
+	}
+
+	public void closeAllWindowWithoutParent(WebDriver driver,String parentID) {
+		Set<String> allWindowIDs = driver.getWindowHandles();
+		for (String id : allWindowIDs) {
+			if (!id.equals(parentID)) {
+				driver.switchTo().window(id);
+				driver.close();
+				sleepInsecond(2);
+			}
+
+		}
+		driver.switchTo().window(parentID);
+	}
+	
+	public void sleepInsecond(long timeInsecond) {
+		try {
+			Thread.sleep(timeInsecond * 1000);
+		} catch (InterruptedException e) {
+
+			e.printStackTrace();
+		}
+	}
 
 }
